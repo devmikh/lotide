2e8b67e5b517e44708b7c47431c45eed34079d4b
Mikhail Serebriakov <mishacyb@gmail.com>
2020-04-29 19:59:12 +0000

    Implement countLetters function

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] === [${expected}]`);
+  }
+};
+
+const countLetters = function(inputString) {
+  const results = {};
+
+  for (let char of inputString) {
+    if (char !== ' ') {
+      if (results[char]) {
+        results[char] += 1;
+      } else {
+        results[char] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+const result = countLetters("lighthouse in the house");
+
+assertEqual(result["l"], 1);
+assertEqual(result["i"], 2);
+assertEqual(result["g"], 1);
+assertEqual(result["h"], 4);
+assertEqual(result["t"], 2);
+assertEqual(result["o"], 2);
+assertEqual(result["u"], 2);
+assertEqual(result["s"], 2);
+assertEqual(result["e"], 3);
+assertEqual(result["n"], 1);
