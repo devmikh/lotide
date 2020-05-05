961b33e280b95e374115ab570f92578d53007c85
Mikhail Serebriakov <mishacyb@gmail.com>
2020-05-05 00:13:17 +0000

    Refactored some functions

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,20 @@
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
+  // check if length of each array is the same
+  if (arr1.length === arr2.length) {
+    // loop through each element in the array 1 and compare to each element in the array 2
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
+      // if both elements are arrays, compare them
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        if (!eqArrays(arr1[i], arr2[i])) {
+          return false;
+        }
+      // if both elements are values, compare them
+      } else if (arr1[i] !== arr2[i]) {
         return false;
       }
     }
+  } else {
+    return false;
   }
   return true;
 };
--- eqObjects.js
+++ eqObjects.js
@@ -1,7 +1,7 @@
 const eqArrays = require('./eqArrays');
 
 const eqObjects = function(object1, object2) {
-  // check if number of keys in each object is the same 
+  // check if number of keys in each object is the same
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (let key in object1) {
       // check if value of each key in both objects is an array
@@ -12,7 +12,9 @@ const eqObjects = function(object1, object2) {
         }
       // check if value of each key in both objects is an object itself
       } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object' && object1[key] !== null && object2[key] !== null) {
-        return eqObjects(object1[key], object2[key]);
+        if (!eqObjects(object1[key], object2[key])) {
+          return false;
+        }
       // if it's neither array nor object, just compare the values
       } else if (object1[key] !== object2[key]) {
         return false;
